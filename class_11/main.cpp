178e7e8a48540f04d9fdaaeae4c9b32635b05acd
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2022-02-15 21:25:38 +0500

    dynammic memory allocation

--- class_11/main.cpp
+++ class_11/main.cpp
@@ -1,51 +1,68 @@
 #include <iostream>
 using namespace std;
 
-class Person{
-    private:
+class Person
+{
+private:
     string _name;
     int _age;
-    public:
+
+public:
     // default constructor
-    Person(){
+    Person()
+    {
         _name = "";
         _age = 0;
     }
     // parameterized constructor
-    Person(string name, int age){
+    Person(string name, int age)
+    {
         _name = name;
         _age = age;
     }
 
     // setter and getter for all private variables using this operator
-    void setName(string name){
+    void setName(string name)
+    {
         this->_name = name;
     }
-    string getName(){
+    string getName()
+    {
         return this->_name;
     }
-    void setAge(int age){
+    void setAge(int age)
+    {
         this->_age = age;
     }
-    int getAge(){
+    int getAge()
+    {
         return this->_age;
     }
     // print function
-    void print(){
+    void print()
+    {
         cout << "Name: " << this->getName() << endl;
         cout << "Age: " << this->getAge() << endl;
     }
 };
 
-int main() { 
-    cout << "Enter Number 1: ";
-    int num1;
-    cin >> num1;
-    cout << "Enter Number 2: ";
-    int num2;
-    cin >> num2;
-    cout << num1 / num2 << endl; 
+int main()
+{
+    cout << "Enter size of array: ";
+    int size;
+    cin >> size;
+    int *arr = new (nothrow) int[size];
+    if (arr == NULL)
+    {
+        cout << "Memory allocation failed" << endl;
+        system("pause");
+        return 0;
+    }
+    // delete allocated memory
+    delete[] arr;
+
+    
 
     system("pause");
-    return 0; 
+    return 0;
 }
