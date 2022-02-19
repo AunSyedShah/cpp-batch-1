6db7b88b0344e03bd0c579591b59d94d4579f7a4
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2022-02-19 21:43:57 +0500

    vector

--- class_13/main.cpp
+++ class_13/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <deque>
 using namespace std;
 
+int main()
+{
+    vector <int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
 
-int main() { 
-    cout << "Hello World!" << endl;
+    vector <int>::iterator it;
+
+    for(it = v.begin(); it < v.end(); it++){
+        cout << *it << endl;
+    }
+
+
 
     system("pause");
-    return 0; 
+    return 0;
 }
