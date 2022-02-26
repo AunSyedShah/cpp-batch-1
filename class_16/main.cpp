d6745aad98dfcf543f10aabcdaf5d232823fbc68
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2022-02-26 20:55:29 +0500

    function template

--- class_16/main.cpp
+++ class_16/main.cpp
@@ -1,29 +1,54 @@
 #include <iostream>
 using namespace std;
 
-// function overloading for integer
-int add(int a, int b)
+// function template for adding two numbers
+template <class T>
+T add(T a, T b)
 {
     return a + b;
 }
 
-// function overloading for float
-float add(float a, float b)
+// function template for calculating percentage
+template <class T>
+T percentage(T a, T b)
 {
-    return a + b;
+    return (a * 100) / b;
 }
 
-// function overloading for double
-double add(double a, double b)
+// function template for printing any value
+template <class T>
+void print(T a)
 {
-    return a + b;
+    cout << a << endl;
+}
+
+template <typename T>
+T getInput()
+{
+    T x;
+    cin >> x;
+    return x;
 }
 
 int main()
 {
+    // integer values
     cout << add(10, 20) << endl;
-    cout << add(10.5, 20.5) << endl;
-    cout << add(10.5, 20.5) << endl;
+    // float values
+    cout << add(10.25, 20.85) << endl;
+    // for strings
+    cout << add('a', 'a') << endl;
+
+    // percentage of two numbers
+    cout << percentage(10, 20) << endl;
+    cout << percentage(10.25, 20.85) << endl;
+
+    print("noor");
+    print(25);
+
+    // get input from user
+    int x;
+    x = getInput<int>();
 
     system("pause");
     return 0;
