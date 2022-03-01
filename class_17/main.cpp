fb886e507cc3c40ba9d6453cea655c4cfd260964
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2022-03-01 20:43:25 +0500

    friend function intro

--- class_17/main.cpp
+++ class_17/main.cpp
@@ -1,67 +1,28 @@
 #include <iostream>
 using namespace std;
 
-// Complex Number Class
-class Complex
+// class to explain the concept of friend function
+class A
 {
 private:
-    double real;
-    double imaginary;
+    int a;
 
 public:
-    Complex(double r = 0, double i = 0)
+    A(int x)
     {
-        real = r;
-        imaginary = i;
+        a = x;
     }
-    void display()
-    {
-        cout << real << " + " << imaginary << "i" << endl;
-    }
-
-    Complex operator+(Complex &c)
-    {
-        Complex temp;
-        temp.real = this->real + c.real;
-        temp.imaginary = this->imaginary + c.imaginary;
-        return temp;
-    }
-    Complex operator-(Complex &c)
-    {
-        Complex temp;
-        temp.real = real - c.real;
-        temp.imaginary = imaginary - c.imaginary;
-        return temp;
-    }
-
-    // Overloading the operator *
-    Complex operator*(Complex &c)
-    {
-        Complex temp;
-        temp.real = (real * c.real) - (imaginary * c.imaginary);
-        temp.imaginary = (real * c.imaginary) + (imaginary * c.real);
-        return temp;
-    }
-    // Overloading the operator /
-    Complex operator/(Complex &c)
-    {
-        Complex temp;
-        temp.real = (real * c.real) + (imaginary * c.imaginary);
-        temp.imaginary = (imaginary * c.real) - (real * c.imaginary);
-        return temp;
-    }
+    friend void display(A obj);
 };
+
+void display(A obj)
+{
+    cout << "Value of a is " << obj.a << endl;
+}
 
 int main()
 {
-    Complex c1(2, 3);
-    Complex c2(3, 4);
-    Complex c3 = c1 + c2;
-    // 5 + 7i
-    c3.display();
-    Complex c4 = c1 - c2;
-    c4.display();
-
+    display(A(10));
     system("pause");
     return 0;
 }
