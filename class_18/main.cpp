dc4ee57eb2103d18e6338bf1520000d201247455
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2022-03-03 20:36:55 +0500

    same feature

---
+++ class_18/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class Mammal
+{
+private:
+    int age;
+
+protected:
+    int weight;
+
+public:
+    Mammal()
+    {
+        cout << "Mammals can give direct birth." << endl;
+    }
+    int getAge()
+    {
+        return age;
+    }
+};
+
+class WingedAnimal
+{
+private:
+    int age;
+public:
+    WingedAnimal()
+    {
+        cout << "Winged animal can flap." << endl;
+    }
+    // get age
+    int getAge()
+    {
+        return age;
+    }
+};
+
+class Bat : public Mammal, public WingedAnimal
+{
+};
+
+int main()
+{
+    Bat b1;
+    b1.getAge();
+
+
+    return 0;
+}
